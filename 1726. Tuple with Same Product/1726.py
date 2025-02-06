518c5f7f0fedb45638220441e1825366cc7812ab
Shreesha R Bhat <65221970+rshreeshabhat@users.noreply.github.com>
2025-02-06 11:53:17 +0530

    1726. Tuple with Same Product(update)

--- 1726. Tuple with Same Product/1726.py
+++ 1726. Tuple with Same Product/1726.py
@@ -1,13 +1,15 @@
-import collections
+from typing import Counter
+
 
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        ans = 0
-        count = collections.Counter()
-
+        s = 0
+        n = []
         for i in range(len(nums)):
             for j in range(i):
-                prod = nums[i] * nums[j]
-                ans += count[prod] * 8
-                count[prod] += 1
-        return ans
+                n.append(nums[i]*nums[j])
+        p= Counter(n)
+        for k in p:
+            if p[k] >1:
+                s += int((p[k]*(p[k]-1))/2)*8
+        return s
